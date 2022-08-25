9b8c52034e31ae42dc00bc5f39ceedfcbd19a625
Valentina <91225414+Avalentorres@users.noreply.github.com>
2022-08-25 16:41:29 -0300

    components

---
+++ NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return ( 
+        <nav>
+         <ol>
+            <li>Vestido</li>
+            <li>Remeras</li>
+            <li>Pantalones</li>
+            <li>Polleras</li>
+        </ol>
+
+        <ul>
+            <li>Bodys</li>
+            <li>Gorras</li>
+            <li>Buzos</li>
+        </ul>
+ </nav>
+       
+    );
+};
+
+export default NavBar 

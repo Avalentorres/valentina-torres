07e47fc8790f05f76a7806f21899ff19f93a0142
Valentina <91225414+Avalentorres@users.noreply.github.com>
2022-08-25 18:55:13 -0300

    src

---
+++ App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Valentina Torres</p>
+        <p>E-commerce</p>
+        
+        <p>
+          Edit <code>Talitha Indumentaria</code> Lo mejor para vos!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Más Información
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
